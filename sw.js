43cc6e0f2494430e103d7c53897c2e09306294af
fuadmd <159006190+fuadmd@users.noreply.github.com>
2026-01-01 23:12:38 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
-const cacheName = 'gold-app-v1';
-const assets = ['./', './index.html'];
+const CACHE_NAME = 'gold-price-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
